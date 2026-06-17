4ad968a3ddf66dd7ae13b365c575909ead4173e5
Jan Zwingmann <janzwingmann@gmail.com>
2026-06-17 12:05:29 +0200

    add lexer skeleton

--- src/token.rs
+++ src/token.rs
@@ -9,3 +9,41 @@ pub enum Token {
     LParen,       // (
     RParen,       // )
 }
+
+pub fn tokenize(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut chars = input.chars().peekable();
+
+    while let Some(&c) = chars.peek() {
+        match c {
+            ' ' | '\t' | '\n' => {
+                chars.next();
+            }
+            '&' => {
+                tokens.push(Token::And);
+                chars.next();
+            }
+            '|' => {
+                tokens.push(Token::Or);
+                chars.next();
+            }
+            '!' => {
+                tokens.push(Token::Not);
+                chars.next();
+            }
+            '(' => {
+                tokens.push(Token::LParen);
+                chars.next();
+            }
+            ')' => {
+                tokens.push(Token::RParen);
+                chars.next();
+            }
+            _ => {
+                chars.next();
+            }
+        }
+    }
+
+    tokens
+}
